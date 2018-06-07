ad731288a0af04abf83a2d927ccd792d4ea19eda
priyanka.harkare@gmail.com <priyanka.harkare@gmail.com>
2018-06-07 19:55:39 -0400

    Crate a basic html frame
    Tried to create a click function for one card
    
    To do:make an array for the cards and add click such that an x or a 0 shows u
    populates on click

---
+++ assets/scripts/events.js
@@ -0,0 +1,12 @@
+const getFormFields = require('../../lib/get-form-fields')
+
+const changeSymbol = function (event) {
+  event.preventDefault()
+  console.log('I need to switch symbol')
+  const data = getFormFields(event.target)
+  console.log('On signIn function data is', data)
+}
+
+module.exports = {
+  changeSymbol: changeSymbol
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,7 +5,14 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./events')
 
 $(() => {
   // your JS code goes here
+})
+//
+// $('.box').on('click', authEvents.switchSymbol)
+
+$(() => {
+  $('#box1').on('click', authEvents.changeSymbol)
 })
